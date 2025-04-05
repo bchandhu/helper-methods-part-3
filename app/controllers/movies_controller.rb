e773505fa4220170aa186293f7d047d58395837d
Chandhu Bhumireddy <89249579+bchandhu@users.noreply.github.com>
2025-04-05 21:30:10 +0000

    updated partial views

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,6 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show, :edit, :update, :destroy]
+
   def new
     @movie = Movie.new
   end
@@ -10,11 +12,12 @@ class MoviesController < ApplicationController
       format.json { render json: @movies }
 
       format.html
+
     end
   end
 
   def show
-    @movie = Movie.find(params.fetch(:id))
+    # @movie = Movie.find(params.fetch(:id))
   end
 
   def create
@@ -23,38 +26,38 @@ class MoviesController < ApplicationController
     if @movie.valid?
       @movie.save
 
-      redirect_to movies_url, notice: "Movie was successfully created."
+      redirect_to movies_url, notice: "Movie created successfully."
     else
       render "new"
     end
   end
 
   def edit
-    @movie = Movie.find(params.fetch(:id))
   end
 
   def update
-    @movie = Movie.find(params.fetch(:id))
-
+    
     if @movie.update(movie_params)
-      redirect_to @movie, notice: "Movie was successfully updated."
+      redirect_to @movie, notice: "Movie updated successfully."
     else
       render "edit"
     end
   end
 
   def destroy
-    @movie = Movie.find(params.fetch(:id))
-
+    
     @movie.destroy
 
-    redirect_to movies_url, notice: "Movie was successfully destroyed."
+    redirect_to movies_url, notice: "Movie deleted successfully."
   end
 
   private
 
   def movie_params
-    params.require(:movie).permit(:title, :description, :image_url)
+    params.require(:movie).permit(:title, :description, :image_url, :released_on)
   end
 
+  def set_movie
+    @movie = Movie.find(params.fetch(:id))
+  end
 end
